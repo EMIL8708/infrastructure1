0473e0e41b70d35ccf36104e21e11ff7b2667a95
EMIL MOLDOISAEV <92470787+EMIL8708@users.noreply.github.com>
2024-01-27 12:17:54 -0600

    updated data

--- ec2/data.tf
+++ ec2/data.tf
@@ -1,3 +1,4 @@
+# Pulls VPC information
 data "terraform_remote_state" "this" {
   backend = "remote"
 
@@ -6,6 +7,19 @@ data "terraform_remote_state" "this" {
 
     workspaces = {
       name = "vpc"
+    }
+  }
+}
+
+# Pulls DB information
+data "terraform_remote_state" "rds" {
+  backend = "remote"
+
+  config = {
+    organization = "moldoisa"
+
+    workspaces = {
+      name = "rds"
     }
   }
 }
